34b1313d69e96276d37f418eed8b057d66ce73e1
Alex Kalmakov <st00nsa@gmail.com>
2015-12-11 00:20:38 +0300

    Added: random card data and lodash lib

--- server/fixture.js
+++ server/fixture.js
@@ -1,7 +1,36 @@
 var Card = MeteorApp.Card;
 
 if (Card.find().count() == 0) {
-    Card.insert({x: 0, y: 0});
-    Card.insert({x: 0, y: 1});
-    Card.insert({x: 0, y: 2});
+    for(var i = 0; i < 6; i++) {
+        var data = {
+            title: _.sample(['Жирный орк', 'Тонкий орк', 'Средний орк']),
+            type: 'creature',
+            text: 'Любит есть',
+            x: _.sample([1, 2, 3]),
+            y: _.sample([1, 2, 3]),
+            dmg: _.sample([1, 2, 3]),
+            health: _.sample([1, 3, 4]),
+            img: [1, 2, 3].map(i => 'card/orc' + i),
+            cardGroup: _.sample(['hand', 'deck', 'table']),
+            ownerId: '1'
+        };
+
+        Card.insert(data);
+    }
+
+    var data = {
+            title: _.sample(['Жирный орк', 'Тонкий орк', 'Средний орк']),
+            type: 'creature',
+            text: 'Любит есть',
+            x: _.sample([1, 2, 3]),
+            y: _.sample([1, 2, 3]),
+            dmg: _.sample([1, 2, 3]),
+            health: _.sample([1, 3, 4]),
+            img: [1, 2, 3].map(i => 'card/orc' + i),
+            cardGroup: 'table',
+            ownerId: '1'
+    };
+
+    Card.insert(data);
+
 }
