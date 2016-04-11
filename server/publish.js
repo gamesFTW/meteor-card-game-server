6c52703cfd48b57bea181b7ca7f88e3b2320037f
Alexey Kalmakov <st00nsa@gmail.com>
2016-04-12 00:40:56 +0300

    Removed autopublish

--- server/publish.js
+++ server/publish.js
@@ -1,8 +1,9 @@
 Meteor.publish('Cards', function() { return MeteorApp.Cards.find() });
 Meteor.publish('Decks', function() { return MeteorApp.Decks.find() });
-Meteor.publish('CardsInGame', function() { return MeteorApp.CardsInGame.find() });
-Meteor.publish('Actions', function() { return MeteorApp.Actions.find() });
+Meteor.publish('CardsInGame', function(gameId) { return MeteorApp.CardsInGame.find({gameId: gameId}) });
+Meteor.publish('Actions', function(gameId) { return MeteorApp.Actions.find({gameId: gameId}) });
 Meteor.publish('Games', function() { return MeteorApp.Games.find() });
+Meteor.publish('Images', function() { return MeteorApp.Images.find() });
 
 MeteorApp.Images.allow({
   'insert': function () {
